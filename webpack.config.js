75122bafc7ba1bc9dc3d86ad07ccc2cf316e350b
renambot <renambot@gmail.com>
2020-12-17 16:13:33 -0600

    new: now with React for realz

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+	entry: './src/app',
+	output: {
+		path: path.resolve(__dirname, 'build'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [{
+			test: /\.js$/,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: ['@babel/preset-env', '@babel/preset-react']
+				}
+			}
+		}]
+	}
+};
